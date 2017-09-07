c1d892e105062ec2493ffb128b497b13f2f100ad
Hilmar Finnsson <hilmarfinnsson@gmail.com>
2017-09-07 22:03:51 +0000

    Boil worker template test

--- workers/boil.py
+++ workers/boil.py
@@ -8,3 +8,8 @@ class BoilWorker(BaseWorker):
     def __init__(self):
         super(BoilWorker, self).__init__()
         log.info('Finson......take it away!')
+
+    def startboil(self, level):
+        log.info('Lets {0} boil!'.format(level))
+        self.call_master_method("message", args=["giggidy"])
+
