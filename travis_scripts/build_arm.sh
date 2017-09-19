e74cf969a799523233ec4b6ef80df3ff15d429eb
Guðmundur Björn Birkisson <gudmundurbb@gagnaveita.is>
2017-09-19 13:24:29 +0000

    Throw error in build scripts

--- travis_scripts/build_arm.sh
+++ travis_scripts/build_arm.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -e
+
 sed 's/\/core:x64/\/core:arm/g' Dockerfile > Dockerfile.arm
 docker run --rm --privileged multiarch/qemu-user-static:register --reset
 docker build -t distribrewed/workers:arm -f Dockerfile.arm .
--- travis_scripts/build_x64.sh
+++ travis_scripts/build_x64.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -e
+
 docker build -t distribrewed/workers:x64 -f Dockerfile .
 docker login -u="$DOCKER_USER" -p="$DOCKER_PASS"
 docker push distribrewed/workers:x64
