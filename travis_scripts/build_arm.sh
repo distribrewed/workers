0f718cdbf844eb04dc253946957edafd3c1dd6c3
Guðmundur Björn Birkisson <gudmundurbb@gagnaveita.is>
2017-10-01 19:06:34 +0000

    Update travis scripts

--- travis_scripts/build_arm.sh
+++ travis_scripts/build_arm.sh
@@ -1,9 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 
 sed 's/\/core:x64/\/core:arm/g' Dockerfile > Dockerfile.arm
 docker run --rm --privileged multiarch/qemu-user-static:register --reset
 docker build -t distribrewed/workers:arm -f Dockerfile.arm .
-docker login -u="$DOCKER_USER" -p="$DOCKER_PASS"
-docker push distribrewed/workers:arm
+
+if [[ ${TRAVIS} == "true" ]]; then
+    docker login -u="$DOCKER_USER" -p="$DOCKER_PASS"
+    docker push distribrewed/workers:arm
+fi
