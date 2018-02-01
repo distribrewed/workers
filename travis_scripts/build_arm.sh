f879466e35acfd71954c1617b0e97caabe613d40
Hreidar Joelsson <hreidar@gmail.com>
2018-02-01 22:27:28 +0000

    Update build_arm.sh

--- travis_scripts/build_arm.sh
+++ travis_scripts/build_arm.sh
@@ -2,7 +2,8 @@
 
 set -e
 
-sed 's/\/core:x64/\/core:arm/g' Dockerfile > Dockerfile.arm
+sed 's/\/core:x64/\/core:arm/g' Dockerfile > Dockerfile.tmp
+sed 's/\/opt/\/usr/g' Dockerfile.tmp > Dockerfile.arm
 docker run --rm --privileged multiarch/qemu-user-static:register --reset
 docker build -t distribrewed/workers:arm -f Dockerfile.arm .
 
